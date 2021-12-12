3a9ad12e558e410690a0dc07d6fd34c9888188bd
hanzy <hans78905@gmail.com>
2021-12-12 23:45:43 +0900

    Add InsertionSort.playground: Incompleted

---
+++ Algorithms/InsertionSort.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+func insertionSort(_ array:[Int]) -> [Int] {
+    
+    for index in 1..<array.count {
+        let value = array[index]
+        var position = index
+        
+        while (position > 0 && array[index - 1] > value) {
+            //insert at the position
+        }
+    }
+    return []
+}
