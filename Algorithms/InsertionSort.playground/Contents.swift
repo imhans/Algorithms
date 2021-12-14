836b1f56581b59a5aaf2d6f919a959304a927c7a
hanzy <hans78905@gmail.com>
2021-12-14 20:40:49 +0900

    Complete InsertionSort.playground

--- Algorithms/InsertionSort.playground/Contents.swift
+++ Algorithms/InsertionSort.playground/Contents.swift
@@ -1,14 +1,37 @@
 import Cocoa
 
+/*
+ Simple sorting algorithm
+ Stable Sort; Bubble, Merge, and Insertion
+ Time Complexity: O(n^2)
+ Space Complexity: O(1)
+ */
 func insertionSort(_ array:[Int]) -> [Int] {
+    guard array.count > 0 else {
+        return array
+    }
     
-    for index in 1..<array.count {
-        let value = array[index]
+    var temp = array
+    
+    for index in 1..<temp.count {
+        let value = temp[index]
         var position = index
         
-        while (position > 0 && array[index - 1] > value) {
-            //insert at the position
+        while (position > 0 && temp[position - 1] > value) {
+            temp.swapAt(position, position - 1)
+            position -= 1
         }
     }
-    return []
+    return temp
 }
+
+var numbers: [Int] = []
+for _ in 1..<10 {
+    numbers.append(Int.random(in: 1..<100))
+}
+
+print("\(numbers)")
+
+let test = insertionSort(numbers)
+
+print("\(test)")
